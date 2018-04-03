044df73113c4e6dca72d5c04fda40c0378358caf
Clement Delafargue <clement@delafargue.name>
2018-04-03 16:55:29 +0200

    JSON serde

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,49 @@
 extern crate reqwest;
+extern crate serde;
+extern crate serde_json;
 
-use reqwest::Error;
+#[macro_use]
+extern crate serde_derive;
 
-fn get_text(uuid: &str) -> Result<String, Error> {
+use std::error::Error;
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Link {
+    href: String,
+    rel: String,
+    title: String,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Talk {
+    id: String,
+    title: String,
+    talk_type: String,
+    track: String,
+    links: Vec<Link>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Speaker {
+    uuid: String,
+    bio_as_html: String,
+    accepted_talks: Vec<Talk>,
+}
+
+fn get_speaker(uuid: &str) -> Result<Speaker, Box<Error>> {
     let url = format!(
         "http://cfp.devoxx.fr/api/conferences/DevoxxFR2018/speakers/{}",
         uuid
     );
     let text = reqwest::get(&url)?.text()?;
-    Ok(text)
+    let s: Speaker = serde_json::from_str(&text)?;
+    Ok(s)
 }
 
 fn main() {
-    let body = get_text("76606cd0-6261-44b2-ad0e-3518a0e66995").unwrap();
-    println!("body = {:?}", body);
+    let s = get_speaker("76606cd0-6261-44b2-ad0e-3518a0e66995").unwrap();
+    println!("{:?}", s);
 }
