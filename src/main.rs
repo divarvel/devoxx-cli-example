9570ca06a3b5f330cd4ea913c669aab651ede919
Clement Delafargue <clement@delafargue.name>
2018-04-03 16:44:06 +0200

    Make user ID a parameter

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,16 @@ extern crate reqwest;
 
 use reqwest::Error;
 
-fn get_text() -> Result<String, Error> {
-    let text = reqwest::get("http://cfp.devoxx.fr/api/conferences/DevoxxFR2018/speakers/76606cd0-6261-44b2-ad0e-3518a0e66995")?.text()?;
+fn get_text(uuid: &str) -> Result<String, Error> {
+    let url = format!(
+        "http://cfp.devoxx.fr/api/conferences/DevoxxFR2018/speakers/{}",
+        uuid
+    );
+    let text = reqwest::get(&url)?.text()?;
     Ok(text)
 }
 
 fn main() {
-    let body = get_text().unwrap();
+    let body = get_text("76606cd0-6261-44b2-ad0e-3518a0e66995").unwrap();
     println!("body = {:?}", body);
 }
