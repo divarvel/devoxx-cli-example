be9b5d358d2b111f94d9f22a776c3026f39e7328
Clement Delafargue <clement@delafargue.name>
2018-04-03 17:14:28 +0200

    Read speaker ID from command line + verbose flag

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,11 @@ extern crate serde_json;
 
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate structopt;
 
 use std::error::Error;
+use structopt::StructOpt;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -33,18 +36,31 @@ struct Speaker {
     accepted_talks: Vec<Talk>,
 }
 
-fn get_speaker(uuid: &str) -> Result<Speaker, Box<Error>> {
+#[derive(StructOpt, Debug)]
+struct Options {
+    #[structopt(short = "v", long = "verbose")]
+    verbose: bool,
+    #[structopt(name = "SPEAKER ID")]
+    speaker_id: String,
+}
+
+fn get_speaker(uuid: &str, verbose: &bool) -> Result<Speaker, Box<Error>> {
     let url = format!(
         "http://cfp.devoxx.fr/api/conferences/DevoxxFR2018/speakers/{}",
         uuid
     );
     let text = reqwest::get(&url)?.text()?;
+    if *verbose {
+        println!("GET {}", &url);
+    }
     let s: Speaker = serde_json::from_str(&text)?;
     Ok(s)
 }
 
 fn main() {
-    let s = get_speaker("76606cd0-6261-44b2-ad0e-3518a0e66995").unwrap();
+    let command = Options::from_args();
+    // "76606cd0-6261-44b2-ad0e-3518a0e66995"
+    let s = get_speaker(&command.speaker_id, &command.verbose).unwrap();
     for t in s.accepted_talks {
         println!("Talk: {}", &t.title)
     }
