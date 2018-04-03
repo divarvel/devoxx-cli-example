1df0257c878adebf4bcefd81350451a73f0fca37
Clement Delafargue <clement@delafargue.name>
2018-04-03 16:42:39 +0200

    First HTTP request

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate reqwest;
+
+use reqwest::Error;
+
+fn get_text() -> Result<String, Error> {
+    let text = reqwest::get("http://cfp.devoxx.fr/api/conferences/DevoxxFR2018/speakers/76606cd0-6261-44b2-ad0e-3518a0e66995")?.text()?;
+    Ok(text)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let body = get_text().unwrap();
+    println!("body = {:?}", body);
 }
